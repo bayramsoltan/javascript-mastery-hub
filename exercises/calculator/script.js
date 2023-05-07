e544dfc596f1ece5d93b6a16e7ffeb992a82ab0d
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-07 23:33:28 +0300

    js-calculator-30%

--- exercises/calculator/script.js
+++ exercises/calculator/script.js
@@ -1,13 +1,34 @@
-const result = eval("4+2*1");
-result.innerHTML = `${result}`;
 
+//numbers
+let number7 = 7;
+number7 = document.querySelector("#seven");
 
-const firstVal = document.querySelector("#numberOne");
-const secondVal = document.querySelector("#numberTwo");
+function show7(){
+    console.log(number7);
+}
+
+const number8 = document.querySelector("#eight");
+const number9 = document.querySelector("#nine");
+const number6 = document.querySelector("#six");
+const number5 = document.querySelector("#five");
+const number4 = document.querySelector("#four");
+const number3 = document.querySelector("#three");
+const number2 = document.querySelector("#two");
+const number1 = document.querySelector("#one");
+const number0 = document.querySelector("#zero");
+
+//signs
+const plusSign = document.querySelector("#plus");
+const minusSign = document.querySelector("#minus");
+const multiplySign = document.querySelector("#multiply");
+const divideSign = document.querySelector("#divide");
+const dotSign = document.querySelector("#dot");
+const equalSign = document.querySelector("#equal");
+
 result = 0;
 
 
 function addNumb(){
             result = Number(firstVal.value) + Number(secondVal.value);
-            resultAll.innerHTML = `Sonuç: ${result}`;
+            result.innerHTML = `${result}`;
         }
