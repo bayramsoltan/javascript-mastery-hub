047f070e5dbd3c63d4ce1338efb3599fbebcd88a
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-13 14:00:15 +0300

    palindrome

--- exercises/calculator/script.js
+++ exercises/calculator/script.js
@@ -1,85 +1,14 @@
-let sums = 0;
-function onScreen(){
-    
+let sum = 0;
+
+function onScreen(value){
+    results.innerHTML +=value;
 }
 
-
-
 function clearAll(){
-    results.innerHtml = "0";
+    results.innerHTML = "";
 }
 
 function calculateResult(){
-
+    sum = eval(results.innerHTML);
+    results.innerHTML = sum;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const minusSign = document.getElementById("minus");
-
-// let sum = "";
-
-//   function show(){
-//     results.innerHTML = minusSign.value;
-// }
-
-// const plusSign = document.querySelector("#plus");
-// function gon(){
-//     results.innerHTML = plusSign.value;
-// }
-
-
-// const aC = document.querySelector("#clear");
-// function clearAll(){
-//     results.innerHTML = 0;
-// }
-
-// //numbers
-// const number8 = document.querySelector("#eight");
-
-// function numberEight(){
-//     results.innerHTML += number8.value;
-// }
-// const number9 = document.querySelector("#nine");
-// function numberNine(){
-//     results.innerHTML += number9.value;
-// }
-
-
-
-
-// const number6 = document.querySelector("#six");
-// const number5 = document.querySelector("#five");
-// const number4 = document.querySelector("#four");
-// const number3 = document.querySelector("#three");
-// const number2 = document.querySelector("#two");
-// const number1 = document.querySelector("#one");
-// const number0 = document.querySelector("#zero");
-
-// //signs
-
-// const multiplySign = document.querySelector("#multiply");
-// const divideSign = document.querySelector("#divide");
-// const dotSign = document.querySelector("#dot");
-// const equalSign = document.querySelector("#equal");
-
-// result = 0;
-
-
-// function addNumb(){
-//             result = Number(firstVal.value) + Number(secondVal.value);
-//             result.innerHTML = `${result}`;
-//         }
