1e882fbfb0a764985f2b67a93967e6db6079bc75
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-15 17:42:31 +0300

    practiced

---
+++ practice/js/practice-16.js
@@ -0,0 +1,16 @@
+//!1- Bir dizideki pozitif ve çift sayıların toplamını ve adedini bulun.
+const arr=[-5,6,3,-5,-7,4,12,-58,72];
+
+let toplam =0;
+let adet =0;
+
+//for way
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>0 && arr[i]%2==0){
+
+//         toplam+= arr[i];
+//         adet++;
+
+//     }
+// }
+
