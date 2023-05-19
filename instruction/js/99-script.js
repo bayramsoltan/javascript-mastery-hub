fa272c81a08f709d4cbcf70827c00da6ab50d591
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-19 23:34:52 +0300

    async

--- instruction/js/99-script.js
+++ instruction/js/99-script.js
@@ -1,10 +1,23 @@
-import {mySort} from "./sort.js"
-
-const arr1 = ["Ali","Bayram","Naz","Ata","Zeki"];
-
-document.getElementById("btn3").addEventListener("click", ()=>{
-    console.log(numberSortASC(arr2));
+import randomMix, {
+  numberSortASC,
+  numberSortDESC,
+  stringSortASC,
+  stringSortDESC,
+} from "./sort.js";
+const arr1 = ["Ali", "Veli", "Selin", "Şener", "Leyla", "Zeki", "Metin"];
+const arr2 = [56, 13, 6, 134, 44, 12];
+document.getElementById("btn1").addEventListener("click", () => {
+  console.log(stringSortASC(arr1));
 });
-document.getElementById("btn4").addEventListener("click", ()=>{
-    console.log(numberSortDESC(arr2));
+document.getElementById("btn2").addEventListener("click", () => {
+  console.log(stringSortDESC(arr1));
+});
+document.getElementById("btn3").addEventListener("click", () => {
+  console.log(numberSortASC(arr2));
+});
+document.getElementById("btn4").addEventListener("click", () => {
+  console.log(numberSortDESC(arr2));
+});
+document.getElementById("btn5").addEventListener("click", () => {
+  console.log(randomMix(arr2));
 });
