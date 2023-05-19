52c4a063171568e359ad18a3b6c74ef0ecdd809a
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-19 22:36:23 +0300

    modules

---
+++ instruction/js/99-script.js
@@ -0,0 +1,10 @@
+import {mySort} from "./sort.js"
+
+const arr1 = ["Ali","Bayram","Naz","Ata","Zeki"];
+
+document.getElementById("btn3").addEventListener("click", ()=>{
+    console.log(numberSortASC(arr2));
+});
+document.getElementById("btn4").addEventListener("click", ()=>{
+    console.log(numberSortDESC(arr2));
+});
---
+++ instruction/js/sort.js
@@ -0,0 +1,13 @@
+/*
+SORT FUNCTION
+If the result is negative, a is sorted before b.
+If the result is positive, b is sorted before a.
+If the result is 0, no changes are done with the sort order of the two values.
+*/
+// arr = [56, 13, 6, 134, 44, 12]
+export const numberSortASC = (arr) => {
+    return arr.sort( (a,b) => a-b );
+}
+export const numberSortDESC = (arr) => {
+    return arr.sort( (a,b) => b-a );
+}
