2f8f7c2590dfe86135c9e03e2c4cea00982ef7db
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-23 00:17:18 +0300

    movie-app

--- instruction/tv-shows/js/tvmaze-api.js
+++ instruction/tv-shows/js/tvmaze-api.js
@@ -1,16 +1,10 @@
 const baseUrl = "https://api.tvmaze.com";
-
-
-
-const searchShows = (query) => {
-  const url = `${baseUrl}/search/shows?q=${query}`;
-  fetch(url)
-  .then((resp) => resp.json())
-  .then((data) =>console.log(data))
-};
-
-
-
-const getShowDetails = () => {};
-
+const searchShows = (query, cb) => {
+    const url = `${baseUrl}/search/shows?q=${query}`;
+    fetch(url)
+    .then( (resp) => resp.json() )
+    .then( (data) => cb(data) )
+}
+const getShowDetails = () =>{
+}
 export { searchShows, getShowDetails };
