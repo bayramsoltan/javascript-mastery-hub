bf4b2a1b37dd6e53a27394f243ec42174cc2939f
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-28 02:09:54 +0300

    weather-api

---
+++ exercises/weather_api/index.js
@@ -0,0 +1,35 @@
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+const city=document.querySelector("#city");
+const descriptionn=document.querySelector("#description");
+const feels=document.querySelector("#feels");
+
+const textInput=document.querySelector("#inputText");
+const btn=document.querySelector("#btn");
+
+btn.addEventListener("click",()=>{
+   const cityName=textInput.value;
+   getData(cityName).then(data => {
+       console.log(data);
+       const {name,main:{temp},weather:[{description}]}=data;
+       console.log(name,temp,description);
+       city.textContent +=name;
+       descriptionn.textContent +=description;
+       feels.textContent+=temp;
+   })
+})
+
+const getData=(name)=>{
+   const apiKey=`aef00181dee1ab9c2ca43739994a52f0`;
+   const baseUrl=`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&lang=tr&units=metric`
+   console.log(baseUrl);
+
+   return new Promise((resolve,reject)=>{
+       fetch(baseUrl)
+       .then((res)=>res.json())
+       .then((data)=>resolve(data))
+       .catch((err)=>reject(err));
+       
+   })
+   
+ 
+}
